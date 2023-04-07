1e35fe82a16f56f37b4b3de38efd8dabec0c8402
tiffany-thinzarlin <tl2853@nyu.edu>
2023-04-07 06:23:51 -0400

    server dependencies and basic mjs files

---
+++ src/app.mjs
@@ -0,0 +1,64 @@
+import express from "express";
+import mongoose from "mongoose";
+import sanitize from "mongo-sanitize";
+import "./db.mjs";
+import bcrypt from "bcryptjs";
+import session from "express-session";
+import path from "path";
+import url from "url";
+import jest from "jest";
+import { startAuthenticatedSession, endAuthenticatedSession } from "./auth.mjs";
+//import cy from "cypress";
+
+const app = express();
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+app.set("view engine", "hbs");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+const User = mongoose.model("User");
+const Review = mongoose.model("Review");
+
+app.get("/", async (req, res) => {
+  res.render("index");
+});
+
+app.get("/reviews", async (req, res) => {
+  try {
+    const reviews = await Review.find();
+    res.render("reviews", { reviews });
+  } catch (error) {
+    throw error;
+  }
+});
+
+app.get("/addReview", async (req, res) => {
+  res.render("addReview");
+});
+
+app.post("/addReview", async (req, res) => {
+  console.log(req.body.bathroom);
+  const newReview = new Review({
+    bathroom: sanitize(req.body.bathroom),
+    review: sanitize(req.body.review),
+    rating: sanitize(req.body.rating),
+  });
+  try {
+    await newReview.save();
+    console.log(newReview);
+    res.redirect("/reviews");
+  } catch (error) {
+    throw error;
+  }
+});
+
+app.listen(process.env.PORT ?? 3000);
---
+++ src/db.mjs
@@ -0,0 +1,106 @@
+import mongoose from "mongoose";
+import slug from "mongoose-slug-updater";
+// is the environment variable, NODE_ENV, set to PRODUCTION?
+import fs from "fs";
+import path from "path";
+import url from "url";
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+let dbconf;
+if (process.env.NODE_ENV === "PRODUCTION") {
+  // if we're in PRODUCTION mode, then read the configration from a file
+  // use blocking file io to do this...
+  const fn = path.join(__dirname, "config.json");
+  const data = fs.readFileSync(fn);
+
+  // our configuration file will be in json, so parse it and set the
+  // conenction string appropriately!
+  const conf = JSON.parse(data);
+  dbconf = conf.dbconf;
+} else {
+  // if we're not in PRODUCTION mode, then use
+  dbconf = "mongodb://localhost/final-project";
+}
+mongoose.connect(dbconf);
+/*
+console.log("Waiting for connection to database...");
+try {
+  await mongoose.connect("mongodb://tl2853:", { useNewUrlParser: true });
+  console.log("Successfully connected to database.");
+} catch (err) {
+  console.log("ERROR: ", err);
+}
+*/
+mongoose.plugin(slug);
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, minLength: 3, maxLength: 20 },
+  password: { type: String, required: true, minLength: 8 },
+  email: { type: String, required: true },
+});
+
+const ReviewSchema = new mongoose.Schema({
+  bathroom: { type: String, required: true, minLength: 3, maxLength: 50 },
+  review: {
+    type: String,
+    maxlength: 500,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+});
+
+mongoose.model("User", UserSchema);
+mongoose.model("Review", ReviewSchema);
+
+/*
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  reviews: [
+    {
+      bathroom: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Bathroom",
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5,
+      },
+      review: {
+        type: String,
+        maxlength: 500,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
+*/
